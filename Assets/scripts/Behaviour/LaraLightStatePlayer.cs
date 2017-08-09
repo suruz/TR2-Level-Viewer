7ac3b9e034fb20a14af14f62c6276af34752ac61
suruz <suruzkento@gmail.com>
2017-08-09 11:23:49 +0600

    Update meta data for unity 3

---
+++ Assets/scripts/Behaviour/LaraLightStatePlayer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class LaraLightStatePlayer : MonoBehaviour {
+	
+	Light light;
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light>();
+		DayNightSystem.AddDayNightEventHandler(UpdateDayNight);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void UpdateDayNight(bool isday)
+	{
+		if(light !=null) light.enabled = isday;
+		Debug.Log(isday);
+	}
+}
