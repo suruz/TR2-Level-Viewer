d5f0a71d0549bae969078edecbad013db90c60a3
suruz <suruzkento@gmail.com>
2017-10-28 19:22:35 +0600

    Lara's swimming and under water effect development

---
+++ Assets/scripts/Environment/WaterEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterEffect : MonoBehaviour {
+
+	// Use this for initialization
+    static Material _Material;
+	void Start ()
+    {
+        _Material = GetComponent<MeshRenderer>().sharedMaterial;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        int uvswitch = (int)Mathf.PingPong(Time.time, 1.99f);
+        if(_Material!=null)
+        {
+            _Material.SetFloat("_UVToggle", uvswitch);
+        }
+ 
+	}
+
+    public static Material GetWaterMaterial()
+    {
+        return _Material;
+    }
+}
