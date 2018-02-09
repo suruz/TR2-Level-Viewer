3b1d80d37448a38f6762e077399a46078911aeba
suruz <suruzkento>
2018-02-09 18:19:53 +0600

    refreshing...

--- Assets/scripts/Layer Defination/MaskedLayer.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class MaskedLayer  
-{
-	public static int Switch = (int)(1 << 8);
-	public static int Player = (int)(1 << 9);
-	public static int Default = (int)(1 << 0);
-}
